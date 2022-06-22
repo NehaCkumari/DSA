5beb4bdb5eec6c19cf8c551b6a1ebbd2e4a83187
Neha Kumari <chandravanshi.neha01@gmail.com>
2022-06-22 23:16:37 +0530

    Add files via upload

--- Matrix/Spiral.py
+++ Matrix/Spiral.py
@@ -1,34 +1,31 @@
-#Given a matrix of size r*c. Traverse the matrix in spiral form
-def spiral(list, row, col):
-    last_col=col-1
-    last_row=row-1
-    r=0
-    c=0
-    res =[]
-    while(r<=last_row and c<=last_col):
-        for i in range(c,last_col+1):
-            res.append(list[r][i])
-        r+=1
-        for j in range(r,last_row+1):
-            res.append(list[j][last_col])
-        last_col-=1
-        if last_row>r:
-            x=last_col
-            while(x>=c):
-                res.append(list[last_row][x])
-                x-=1
-            last_row-=1
-        if last_col>c:
-            y=last_row
-            while(y>=r):
-                res.append(list[y][c])
-                y-=1
-            c+=1
-    return res
+def spiral(mat):
+    R=len(mat[0])
+    B=len(mat)
+    L=0
+    T=0
+    while(L!=R and T!=B):
+        i=L
+        while i<R:
+            print(mat[T][i])
+            i+=1
+        T+=1
+        j=T
+        while j<B:
+            print(mat[j][R-1])
+            j+=1
+        R-=1
+        k=R-1
+        while k>=L:
+            print(mat[B-1][k])
+            k-=1
+        B-=1
+        z=B-1
+        while z>=T:
+            print(mat[z][L])
+            z-=1
+        L+=1
+    return
 
-l1 =[[]]
-print(spiral(l1,0,0))
-
-
-
+mat = [[1,2,3],[4,5,6],[7,8,9],[11,12,13]]
+spiral(mat)
 
